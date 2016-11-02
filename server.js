50cdcec534198cceb528d847bb8712c3bc9ff12f
Ashok Pancily Poothiyot <ashok.poothiyot@yale.edu>
2016-11-02 20:49:09 +0000

    created express server.js, package.json

---
+++ server.js
@@ -0,0 +1,17 @@
+/*var express = require('express');
+var app = express();
+
+app.get('/', function(request, response){
+    response.send('Hello MGT-656!');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('gentle-leopard');
+});
+
+app.listen(process.env.PORT || 8080);*/
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World from Cloud9\n');
+}).listen(process.env.PORT);
