2e65ba215ae1ecc4b090a8c408af30a8d6e62705
Ashok Pancily Poothiyot <ashok.poothiyot@yale.edu>
2016-11-06 15:33:55 +0000

    trying to set up mongodb

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,8 +1,48 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer(); 
 var app = express();
 
-app.get('/', function(req, res){
-    res.send("Hello world!");
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(upload.array()); // for parsing multipart/form-data
+app.use(express.static('public'));
+
+app.post('/', function(req, res){
+    console.log(req.body);
+    res.send("recieved your request!");
 });
 
+/*
+app.use(express.static('public'));
+
+app.get('/components', function(req, res){
+    res.render('content');
+});
+*/
+
+/*var things = require('./things.js'); 
+//both index.js and things.js should be in same directory
+app.use('/things', things); 
+
+app.get('/first_template', function(req, res){
+    res.render('first_view');
+});
+
+app.get('/dynamic_view', function(req, res){
+    res.render('dynamic', {
+        name: "TutorialsPoint", 
+        url:"http://www.tutorialspoint.com"
+    });
+   
+});
+
+app.get('/test/:name/:id', function(req, res){
+    res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
+});
+*/
 app.listen(process.env.PORT);
